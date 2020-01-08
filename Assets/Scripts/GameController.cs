c18a353bffa0fcb04b6655bfc5cc8f02726b13ef
Franco Achacoso <fachacoso@berkeley.edu>
2020-01-07 18:05:36 -0800

    Initialize code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int whoTurn; // 0 = X, 1 = O
+    public int turnCount; // num of turns
+    public GameObject[] turnIcons; //display whose turn it is
+    public Sprite[] playIcon;  // 0 = X icon, 1 = O icon
+    public Button[] tictactoeSpaces; // playable space for game
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameSetup();
+    }
+
+    void GameSetup()
+    {
+        whoTurn = 0;
+        turnCount = 0;
+        turnIcons[0].SetActive(true);
+        turnIcons[1].SetActive(false);
+        for (int i = 0; i < tictactoeSpaces.Length; i++)
+        {
+            tictactoeSpaces[i].interactable = true;
+            tictactoeSpaces[i].GetComponent<Image>().sprite = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # tictactoe
 
-This project is an introduction for me to C# and Unity.  Here I'll be trying to recreate this simple yet classic game of tic-tac-toe.
+This project is an introduction for me to C# and Unity.  
+Here I'll be trying to recreate this simple yet classic game of tic-tac-toe.
