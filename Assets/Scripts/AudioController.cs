4da0b01c1962216fb1d75d3e66c6a4ba47e708aa
Franco Achacoso <fachacoso@berkeley.edu>
2020-01-09 03:35:12 -0800

    Added audio mixing between bg music and sfx Still need to make sure slider positions stay consistent

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -1,10 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioController : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     public AudioSource click;
+
+    public void setMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("master", volume);
+    }
+
+    public void setBgVolume(float volume)
+    {
+        audioMixer.SetFloat("bg", volume);
+    }
+
+    public void setSfxVolume(float volume)
+    {
+        audioMixer.SetFloat("sfx", volume);
+    }
 
     public void clickAudio()
     {
