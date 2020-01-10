0300b9ecf2b776c45c74e945d0bdaee9ec0de3cd
Franco Achacoso <fachacoso@berkeley.edu>
2020-01-10 01:55:26 -0800

    Added AI code, modified gameController Still need to test AI code

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI
+{
+    static System.Random random = new System.Random();
+
+    public static int Easy(int[] board)
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < board.Length; i++)
+        {
+            if (board[i] != 0)
+                list.Add(i);
+        }
+        return random.Next(list.Count);
+    }
+
+    public int Impossible(int[] board, int whoseTurn)
+     {
+        if (whoseTurn == -1)
+            return Minimize(board, int.MinValue, int.MaxValue, 9).Item1;
+        else
+            return Maximize(board, int.MinValue, int.MaxValue, 9).Item1;
+    }
+
+
+
+    Tuple<int, int> Minimize(int[] board, int alpha, int beta, int depth)
+    {
+
+        List<int> openSpots = new List<int>();
+        for (int i = 0; i < board.Length; i++)
+        {
+            if (board[i] == 0)
+                openSpots.Add(i);
+        }
+        int bestMoveSoFar = 0;
+        int bestScoreSoFar = int.MaxValue;
+
+        if (depth == 0 || openSpots.Count == 0)
+        {
+            for (int i = 0; i < openSpots.Count; i++)
+            {
+                int[] possibleBoard = board;
+                possibleBoard[i] = -1;
+                int possibleScore = heuristic(possibleBoard);
+                if (possibleScore <= bestScoreSoFar)
+                {
+                    bestMoveSoFar = i;
+                    bestScoreSoFar = possibleScore;
+                    beta = Math.Min(beta, bestMoveSoFar);
+                    if (beta <= alpha)
+                        break;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < openSpots.Count; i++)
+            {
+                int[] possibleBoard = board;
+                possibleBoard[i] = -1;
+                Tuple<int, int> possibleMove = Maximize(possibleBoard, alpha, beta, depth - 1);
+                if (possibleMove.Item2 <= bestScoreSoFar)
+                {
+                    bestMoveSoFar = i;
+                    bestScoreSoFar = possibleMove.Item2;
+                    beta = Math.Min(beta, bestMoveSoFar);
+                    if (beta <= alpha)
+                        break;
+                }
+            }
+        }
+        return new Tuple<int, int>(bestMoveSoFar, bestScoreSoFar);
+    }
+
+    Tuple<int, int> Maximize(int[] board, int alpha, int beta, int depth)
+    {
+
+        List<int> openSpots = new List<int>();
+        for (int i = 0; i < board.Length; i++)
+        {
+            if (board[i] == 0)
+                openSpots.Add(i);
+        }
+        int bestMoveSoFar = 0;
+        int bestScoreSoFar = int.MinValue;
+
+        if (depth == 0 || openSpots.Count == 0)
+        {
+            for (int i = 0; i < openSpots.Count; i++)
+            {
+                int[] possibleBoard = board;
+                possibleBoard[i] = 1;
+                int possibleScore = heuristic(possibleBoard);
+                if (possibleScore >= bestScoreSoFar)
+                {
+                    bestMoveSoFar = i;
+                    bestScoreSoFar = possibleScore;
+                    alpha = Math.Max(alpha, bestScoreSoFar);
+                    if (beta <= alpha)
+                        break;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < openSpots.Count; i++)
+            {
+                int[] possibleBoard = board;
+                possibleBoard[i] = 1;
+                Tuple<int, int> possibleMove = Minimize(possibleBoard, alpha, beta, depth - 1);
+                if (possibleMove.Item2 >= bestScoreSoFar)
+                {
+                    bestMoveSoFar = i;
+                    bestScoreSoFar = possibleMove.Item2;
+                    alpha = Math.Max(alpha, bestScoreSoFar);
+                    if (beta <= alpha)
+                        break;
+                }
+            }
+        }
+        return new Tuple<int, int>(bestMoveSoFar, bestScoreSoFar);
+    }
+
+
+    public int heuristic(int[] board)
+    {
+        int score = 0;
+        int s1 = board[0] + board[1] + board[2];
+        int s2 = board[3] + board[4] + board[5];
+        int s3 = board[6] + board[7] + board[8];
+        int s4 = board[0] + board[3] + board[6];
+        int s5 = board[1] + board[4] + board[7];
+        int s6 = board[2] + board[5] + board[8];
+        int s7 = board[0] + board[4] + board[8];
+        int s8 = board[2] + board[4] + board[6];
+        int[] solutions = { s1, s2, s3, s4, s5, s6, s7, s8 };
+        for (int i = 0; i < solutions.Length; i++)
+        {
+            if (solutions[i] == -3)
+            {
+                score = int.MinValue;
+                break;
+            }
+            else if (solutions[i] == 3)
+            {
+                score = int.MaxValue;
+                break;
+            }
+            else
+            {
+                score += solutions[i];
+            }
+        }
+        return score;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,30 +7,31 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public int whoseTurn; // -1 = X, 1 = O
+    public int whoseTurn; // 0 = X, 1 = O
     public int turnCount; // num of turns
     public GameObject[] turnIcons; //display whose turn it is
     public Sprite[] playerIcons;  // 0 = X icon, 1 = O icon
     public Button[] tictactoeSpaces; // playable space for game
-    public int[] markedSpace; // place piece for each space
+    public int[] board;// place piece for each space
     public GameObject[] winningLines;
     public Text winningText;
     public GameObject winningPannel;
     public Button xButton, oButton;
     public int xScore, oScore;
     public Text xScoreText, oScoreText;
+    public Boolean[] isAI;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        turnIcons[0].SetActive(true);
-        turnIcons[1].SetActive(false);
         GameSetup();
     }
 
     void GameSetup()
     {
-        whoseTurn = PlayerPrefs.GetInt("whoStarts", -1);
+        whoseTurn = PlayerPrefs.GetInt("whoStarts", 0);
+        setCurrent(whoseTurn);
         turnCount = 0;
         xButton.interactable = true;
         oButton.interactable = true;
@@ -39,15 +40,10 @@ public class GameController : MonoBehaviour
             tictactoeSpaces[i].interactable = true;
             tictactoeSpaces[i].GetComponent<Image>().sprite = null;
         }
-        for (int i = 0; i < markedSpace.Length; i++)
+        for (int i = 0; i < board.Length; i++)
         {
-            markedSpace[i] = 0;
+            board[i] = 0;
         }
-        for (int i = 0; i < winningLines.Length; i++)
-        {
-            winningLines[i].SetActive(false);
-        }
-
     }
 
     public void TicTacToeButton(int spaceNumber)
@@ -58,24 +54,37 @@ public class GameController : MonoBehaviour
 
         switch (whoseTurn)
         {
-            case -1:
+            case 0:
                 tictactoeSpaces[spaceNumber].image.sprite = playerIcons[0];
-                markedSpace[spaceNumber] = -1;
-                switchPlayer(1);
+                board[spaceNumber] = -1;
+                if (checkWinner())
+                    return;
+                setCurrent(1);
                 break;
             case 1:
                 tictactoeSpaces[spaceNumber].image.sprite = playerIcons[1];
-                markedSpace[spaceNumber] = 1;
-                switchPlayer(-1);
+                board[spaceNumber] = 1;
+                if (checkWinner())
+                    return;
+                setCurrent(0);
                 break;
             default:
                 Console.WriteLine("Default case");
                 break;
 
         }
+
         turnCount++;
+
         if (turnCount == 9)
+        {
             tie();
+            return;
+        }
+
+        if (isAI[whoseTurn])
+            TicTacToeButton(AI.Easy(board));
+
     }
 
     void tie()
@@ -87,33 +96,33 @@ public class GameController : MonoBehaviour
         }
     }
 
-    int checkWinner()
+    Boolean checkWinner()
     {
-        int s1 = markedSpace[0] + markedSpace[1] + markedSpace[2];
-        int s2 = markedSpace[3] + markedSpace[4] + markedSpace[5];
-        int s3 = markedSpace[6] + markedSpace[7] + markedSpace[8];
-        int s4 = markedSpace[0] + markedSpace[3] + markedSpace[6];
-        int s5 = markedSpace[1] + markedSpace[4] + markedSpace[7];
-        int s6 = markedSpace[2] + markedSpace[5] + markedSpace[8];
-        int s7 = markedSpace[0] + markedSpace[4] + markedSpace[8];
-        int s8 = markedSpace[2] + markedSpace[4] + markedSpace[6];
+        int s1 = board[0] + board[1] + board[2];
+        int s2 = board[3] + board[4] + board[5];
+        int s3 = board[6] + board[7] + board[8];
+        int s4 = board[0] + board[3] + board[6];
+        int s5 = board[1] + board[4] + board[7];
+        int s6 = board[2] + board[5] + board[8];
+        int s7 = board[0] + board[4] + board[8];
+        int s8 = board[2] + board[4] + board[6];
         int[] solutions = { s1, s2, s3, s4, s5, s6, s7, s8 };
         for (int i = 0; i < solutions.Length; i++)
         {
             if (solutions[i] == -3 || solutions[i] == 3)
             {
                 displayWinner(i);
-                return whoseTurn;
+                return true;
             }
         }
-        return 0;
+        return false;
     }
 
     void displayWinner(int solutionNumber)
     {
         winningPannel.SetActive(true);
         winningLines[solutionNumber].SetActive(true);
-        if (whoseTurn == -1)
+        if (whoseTurn == 0)
         {
             xScore++;
             xScoreText.text = xScore.ToString();
@@ -135,6 +144,10 @@ public class GameController : MonoBehaviour
         {
             winningLines[i].SetActive(false);
         }
+        for (int i = 0; i < winningLines.Length; i++)
+        {
+            winningLines[i].SetActive(false);
+        }
     }
 
     public void restart()
@@ -146,25 +159,33 @@ public class GameController : MonoBehaviour
         oScoreText.text = oScore.ToString();
     }
 
-    public void switchPlayer(int player)
+    public void setCurrent(int player)
     {
-        if (checkWinner() != 0)
+        if (player == 0)
         {
-            return;
-        }
-        if (player == -1)
-        {
-            whoseTurn = -1;
-            PlayerPrefs.SetInt("whoStarts", -1);
+            whoseTurn = 0;
             turnIcons[0].SetActive(true);
             turnIcons[1].SetActive(false);
         }
         else
         {
             whoseTurn = 1;
-            PlayerPrefs.SetInt("whoStarts", 1);
             turnIcons[1].SetActive(true);
             turnIcons[0].SetActive(false);
+        }
+
+    }
+
+    public void setFirst(int player)
+    {
+        setCurrent(player);
+        if (player == 0)
+        {
+            PlayerPrefs.SetInt("whoStarts", 0);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("whoStarts", 1);
         }
 
     }
